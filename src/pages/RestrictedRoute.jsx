d8bb96ef1ba30dd33a62b54c9164d54339c8f7ea
SeredaOleksandr <154976460+SeredaOleksandr@users.noreply.github.com>
2024-08-16 21:47:06 +0300

    add location

--- src/pages/RestrictedRoute.jsx
+++ src/pages/RestrictedRoute.jsx
@@ -1,12 +1,13 @@
 import { useSelector } from 'react-redux';
 import { selectIsLoggedIn } from '../redux/auth/selectors';
-import { Navigate } from 'react-router-dom';
+import { Navigate, useLocation } from 'react-router-dom';
 // або PublicRoute
 export const RestrictedRoute = ({ children }) => {
   const isLoggedIn = useSelector(selectIsLoggedIn);
+  const location = useLocation();
 
   if (isLoggedIn) {
-    return <Navigate to="/" />;
+    return <Navigate to={location?.state ?? '/'} />;
   }
 
   return children;
